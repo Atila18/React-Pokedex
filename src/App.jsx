a46eacad939ab23b36ccdbbc67bdaea2728eb90f
Cassandra <kassy.loulou61@gmail.com>
2023-03-29 17:20:30 +0200

    add 3 pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
   },
   {
-    color: "feu",
+    color: "feu-vol",
     name: "Dracaufeu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
@@ -65,12 +65,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png",
   },
   {
+    color: "insecte",
+    name: "Crysacier",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/11.png",
+  },
+  {
+    color: "insecte-vol",
+    name: "Papillusion",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/12.png",
+  },
+  {
+    color: "insecte-poison",
+    name: "Aspicot",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/13.png",
+  },
+  {
+    color: "insecte-poison",
+    name: "Coconfort",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/14.png",
+  },
+  {
+    color: "insecte-poison",
+    name: "Dardagnan",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/15.png",
+  },
+  {
     color: "electrique",
     name: "Pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
+    color: "psy",
     name: "Mew",
   },
 ];
