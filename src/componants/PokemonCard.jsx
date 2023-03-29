b991abaaecc29c9488908025a34bd0e6fb9c9bf7
Cassandra <kassy.loulou61@gmail.com>
2023-03-29 10:09:40 +0200

    Add propTypes

--- src/componants/PokemonCard.jsx
+++ src/componants/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
   return (
     <figure>
@@ -10,5 +12,12 @@ function PokemonCard(props) {
     </figure>
   );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
 
 export default PokemonCard;
